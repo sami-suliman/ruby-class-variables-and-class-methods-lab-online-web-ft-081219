f48bcb5f0841a11cf6b37e540f653d20fb28a29f
sami suliman <sami.bco@gmail.com>
2019-08-25 05:47:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song 
   attr_accessor :name, :artist, :genre 
   @@count = 0
-  @@artist = 0
+  @@artist = []
   def initialize (name, artist, genre)
     @name = name 
     @artist = artist 
